10b11149dc06d429ab9a1f18ed37905402ef3e25
Benjamin <hahnehead@yahoo.com>
2016-09-18 23:27:13 -0400

    added additional nodes to graph

--- run.py
+++ run.py
@@ -12,7 +12,7 @@ def buildSampleNetwork():
     sampleNet = network("sampleNet")
     
     #create sample nodes (players)  [nodeID]
-    a,b,c,d,e,f='abcdef'
+    a,b,c,d,e,f,g='abcdefg'
     
     #Add new players to network
     sampleNet.addNodeById(a)
@@ -21,8 +21,12 @@ def buildSampleNetwork():
     sampleNet.addNodeById(d)
     sampleNet.addNodeById(e)
     sampleNet.addNodeById(f)
+    sampleNet.addNodeById(g)
     
     #create some games played between these players [playerId,player2Id,player1Wins,player2Wins,date]
+    gameAF_1 = game(a,f,9,5,datetime(2016,1,1))
+    gamesAF = [gameAF_1]
+    
     gameAB_1 = game(a,b,9,1,datetime(2016,1,1))
     gamesAB = [gameAB_1]
     
@@ -35,18 +39,37 @@ def buildSampleNetwork():
     gamesCE_1 = game(c,e,9,1,datetime(2016,1,1))
     gamesCE = [gamesCE_1]
     
+    gamesCG_1 = game(c,g,1,9,datetime(2016,1,1))
+    gamesCG = [gamesCG_1]
+    
+    gamesEG_1 = game(e,g,1,9,datetime(2016,1,1))
+    gamesEG = [gamesEG_1]
+    
+    gamesDG_1 = game(d,g,1,9,datetime(2016,1,1))
+    gamesDG = [gamesDG_1]
+    
+    
     
     #create edges between nodes using games played
+    af = edge(a+' vs '+f,a,f,gamesAF)
     ab = edge(a+' vs '+b,a,b,gamesAB)
     cd = edge(c+' vs '+d,c,d,gamesCD)
     bc = edge(b+' vs '+c,b,c,gamesBC)
     ce = edge(c+' vs '+e,c,e,gamesCE)
+    cg = edge(c+' vs '+g,c,g,gamesCG)
+    eg = edge(e+' vs '+g,e,g,gamesEG)
+    dg = edge(d+' vs '+g,d,g,gamesDG)
     
     #Add Edges to network
+    sampleNet.addEdge(af)
     sampleNet.addEdge(ab)
     sampleNet.addEdge(cd)
     sampleNet.addEdge(bc)
     sampleNet.addEdge(ce)
+    sampleNet.addEdge(cg)
+    sampleNet.addEdge(eg)
+    sampleNet.addEdge(dg)
+    
     return sampleNet
     
     
@@ -54,7 +77,7 @@ def buildSampleNetwork():
 if __name__ == "__main__":
     a=buildSampleNetwork()
     print a.toString()
-    for i in range(5000):
+    for i in range(1000):
         a.runPageRank()
     print a.toString()
     
