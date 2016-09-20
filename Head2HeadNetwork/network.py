a019e7ba69576bd5389c17e447a8808a42c479bf
tangerine007 <hahnehead@yahoo.com>
2016-09-20 17:46:31 -0400

    instructions for how to proceed at top of network.py

--- Head2HeadNetwork/network.py
+++ Head2HeadNetwork/network.py
@@ -5,9 +5,17 @@ Created on Sun Sep 18 14:45:35 2016
 @author: Ben
 """
 
+#TODO: restructure network in the following way...
+"""
+1) addGames:
+    a) add nodes by id if they do not exist
+    b) add edge between nodes if one does not exist
+    c) add game to edge
+"""
 
-from node import node
-from game import game
+
+from node import node as Node
+from game import game as Game
 from datetime import datetime
 from math import sqrt
 import os
@@ -17,16 +25,26 @@ class network:
         self.name = name
         self.nodes = {}
         self.edges = {}
+        self.games = {}
         
     ###GETTER_METHODS###  
     def getNodes(self):
         return self.nodes
     def getEdges(self):
         return self.edges
+    def getGames(self):
+        return self.games
     def getNodeById(self,nodeId):
         return self.nodes[nodeId]
     def getEdgeById(self,edgeId):
         return self.edges[edgeId]
+    def getGameById(self,edgeId,gameId):
+        returnGame = [game for game in self.games[edgeId] if game.getGameID()==gameId]
+        if len(returnGame)==1:
+            return returnGame
+        else:
+            print "No game by that ID exists"
+            return game(-1,-1,-1,-1,-1,datetime.now())
         
     ###SETTER_METHODS###
     def setNodes(self,nodes):
@@ -35,7 +53,10 @@ class network:
         self.edges={}
         for edge in edges:
             self.addEdge(edge)
-                 
+    def setGames(self,games):
+        self.games={}
+        for game in games:
+            #TODO: add games here
     
     ###MUTATOR_METHODS###
     def populateNetworkFromGamesCSV(self,nameIn,path=os.path.split(os.path.abspath(__file__))[0],filename=os.path.split(os.path.abspath(__file__))[1]):
@@ -45,7 +66,7 @@ class network:
             for line in f:
                 if not header:
                     p1,p2,p1w,p2w,y,m,d = line[:-1].split(",")
-                    tempGame = game(p1,p2,p1w,p2w,datetime(y,m,d))
+                    tempGame = Game(p1,p2,p1w,p2w,datetime(y,m,d))
                 header=False
         
     def addNodeById(self,nodeId):
@@ -53,17 +74,24 @@ class network:
         if self.nodes.has_key(nodeId):
             cont = input("Node Already Exists, delete and replace?('yes'/'no')")
         if cont=='yes':  
-            self.nodes[nodeId] = node(nodeId)
+            self.nodes[nodeId] = Node(nodeId)
             
     def removeNodeById(self,nodeId):
         del self.nodes[nodeId]
         
     def addEdge(self,edge):
+        #TODO: check if edge exists, might want to remove this method completely and only add
+        #things by node and by game
         self.edges[edge.getId()] = edge
         for nodeID in edge.getNodeIds():
             if not self.nodes.has_key(nodeID):
                 self.addNodeById(nodeID,[])
             self.nodes[nodeID].addEdgeId(edge.getId())
+            
+    def addGame(self,game):
+        p1 = game.getPlayerAId()
+        p2 = game.getPlayerBId()
+        self.edges[p1]
                 
     def removeEdge(self,edgeId):
          del self.edges[edgeId]
