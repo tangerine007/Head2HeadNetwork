8195ec5224ca7a57fa17efe74854b2bf8d433bdd
Benjamin <hahnehead@yahoo.com>
2016-09-20 22:10:55 -0400

    edge now contains list of gameIds instead of actual games

--- Head2HeadNetwork/network.py
+++ Head2HeadNetwork/network.py
@@ -8,6 +8,7 @@ Created on Sun Sep 18 14:45:35 2016
 
 
 from node import node as Node
+from edge import edge as Edge
 from game import game as Game
 from datetime import datetime
 from math import sqrt
@@ -31,12 +32,11 @@ class network:
         return self.nodes[nodeId]
     def getEdgeById(self,edgeId):
         return self.edges[edgeId]
-    def getGameById(self,edgeId,gameId):
-        returnGame = [game for game in self.games[edgeId] if game.getGameID()==gameId]
-        if len(returnGame)==1:
-            return returnGame
+    def getGamesByEdgeId(self,edgeId):
+        if not self.edges.has_key(edgeId):
+            return self.edges[edgeId]
         else:
-            print "No game by that ID exists"
+            print "No edge by that ID exists"
         
     ###SETTER_METHODS###
     def setNodes(self,nodes):
@@ -48,58 +48,74 @@ class network:
     def setGames(self,games):
         self.games={}
         for game in games:
-            #TODO: add games here
+            self.addGame(game)
     
     ###MUTATOR_METHODS###
-    def populateNetworkFromGamesCSV(self,nameIn,path=os.path.split(os.path.abspath(__file__))[0],filename=os.path.split(os.path.abspath(__file__))[1]):
-        header=True
-        sampleNet = network(nameIn)
+    def populateNetworkFromGamesCSV(self,path=os.path.split(os.path.abspath(__file__))[0],filename=os.path.split(os.path.abspath(__file__))[1],header=True):
         with open(os.path.join(path,filename),'r') as f:
             for line in f:
                 if not header:
                     p1,p2,p1w,p2w,y,m,d = line[:-1].split(",")
-                    tempGame = Game(p1,p2,p1w,p2w,datetime(y,m,d))
+                    self.addGame(Game(p1,p2,p1w,p2w,datetime(y,m,d)))
                 header=False
         
     def addNodeById(self,nodeId):
-        cont = 'yes'
+        cont = 'no'
         if self.nodes.has_key(nodeId):
             cont = input("Node Already Exists, delete and replace?('yes'/'no')")
         if cont=='yes':  
             self.nodes[nodeId] = Node(nodeId)
             
-    def removeNodeById(self,nodeId):
-        del self.nodes[nodeId]
-        
     def addEdge(self,edge):
-        #TODO: check if edge exists, might want to remove this method completely and only add
-        #things by node and by game
-        self.edges[edge.getId()] = edge
-        for nodeID in edge.getNodeIds():
-            if not self.nodes.has_key(nodeID):
-                self.addNodeById(nodeID,[])
-            self.nodes[nodeID].addEdgeId(edge.getId())
-            
+        p1 = edge.getNodeAid()
+        p2 = edge.getNodeBid()
+        if p1==p2:
+            print "Can't play game against oneself!"
+            return
+        sortedIds = sorted([p1,p2])
+        edgeId = "-vs-".join(sortedIds);
+        if self.edges.has_key(edgeId):
+            cont = 'no'
+            if self.edges.has_key(edgeId):
+                cont = input("Edge Already Exists, delete and replace?('yes'/'no')")
+            if cont!='yes':  
+                return
+        for nodeId in [p1,p2]:
+            if not self.nodes.has_key(nodeId):
+                self.nodes[nodeId] = Node(nodeId)
+                self.nodes[nodeId].addEdgeId(edgeId)
+
+        self.edges[edgeId] = edge
+        self.games[edgeId] = edge.getGameIds()
+    
     def addGame(self,game):
         p1 = game.getPlayerAId()
         p2 = game.getPlayerBId()
         if p1==p2:
             print "Can't play game against oneself!"
             return
-        for nodeid in [p1,p2]:
-            if not self.nodes.has_key(nodeid):
-                self.nodes[nodeId] = Node(nodeId)
         sortedIds = sorted([p1,p2])
         edgeId = "-vs-".join(sortedIds);
-        
+        for nodeId in [p1,p2]:
+            if not self.nodes.has_key(nodeId):
+                self.nodes[nodeId] = Node(nodeId)
+                self.nodes[nodeId].addEdgeId(edgeId)
+
         if not self.edges.has_key(edgeId):
-            self.edges[edgeId] = edge(edgeId,sortedIds[0],sortedIds[1],[edgeId+"#0"])
+            self.edges[edgeId] = Edge(edgeId,sortedIds[0],sortedIds[1],[edgeId+"#0"])
         else:
             self.edges[edgeId].addGame([edgeId+"#"+`len(self.edges[edgeId].getGames())`])
+     #TODO: Removing a node completely from the network will be a bit more complicated than this, have to remove it from
+     #Everywhere
+#    def removeNodeById(self,nodeId):
+#        del self.nodes[nodeId]
         
+          
+    #TODO: removeEdge method - remove all places edge is referenced (remove edge from list in both nodes too!)
+    #def removeEdgeById(self,edgeId):
+                
+    
         
-    def removeEdge(self,edgeId):
-         del self.edges[edgeId]
     
     def runPageRank(self,useSetAlpha=False,alpha=.5):
         newPR_part1 = (1-alpha)/(len(self.nodes))+alpha
